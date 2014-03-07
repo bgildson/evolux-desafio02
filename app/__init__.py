86b5bb5622109c97f34c8dae76f3792e4784e283
Gildson <Gildson13@hotmail.com>
2014-03-07 09:20:48 -0300

    old dao

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-global app
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
-import views
+from app import views, models
 import daoDB
--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import os
+
 # formularios
 CSRF_ENABLED = True
 SECRET_KEY = 'evolux-desafio-csrf-key'
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # banco de dados
 SQLALCHEMY_DATABASE_NAME = 'app'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app/' + SQLALCHEMY_DATABASE_NAME + '.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') #'sqlite:///' + '.db'
